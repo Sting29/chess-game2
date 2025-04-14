c50220cebeb032b547355172aa3340b37168345f
Konstantin Beregovoy <beregovyyk@gmail.com>
2025-04-14 08:07:34 +0300

    add fount WendyOne

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { AppRouter } from "./routes";
 import "./App.css";
+import { GlobalStyles } from "./styles/GlobalStyles";
 
 function App() {
   return (
     <div className="app">
+      <GlobalStyles />
       <AppRouter />
     </div>
   );
