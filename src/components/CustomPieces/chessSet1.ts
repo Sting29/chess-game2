1249b2c8e8fab6f5f859434e0ddfcb6ffe83bf22
Konstantin Beregovoy <beregovyyk@gmail.com>
2025-07-06 21:38:11 +0300

    add new images for king and queen

--- src/components/CustomPieces/chessSet1.ts
+++ src/components/CustomPieces/chessSet1.ts
@@ -3,16 +3,16 @@ import wP from "../../assets/figures/pawn_white_classic.png";
 import wN from "../../assets/figures/knight_white_classic.png";
 import wB from "../../assets/figures/bishop_white_classic.png";
 import wR from "../../assets/figures/rook_white_classic.png";
-import wQ from "../../assets/figures/queen_white_classic.png";
-import wK from "../../assets/figures/king_white_classic.png";
+import wQ from "../../assets/figures/queen_white_classic_v1.png";
+import wK from "../../assets/figures/king_white_classic_v1.png";
 
 // Black pieces
 import bP from "../../assets/figures/pawn_dark_classic.png";
 import bN from "../../assets/figures/knight_dark_classic.png";
 import bB from "../../assets/figures/bishop_dark_classic.png";
 import bR from "../../assets/figures/rook_dark_classic.png";
-import bQ from "../../assets/figures/queen_dark_classic.png";
-import bK from "../../assets/figures/king_dark_classic.png";
+import bQ from "../../assets/figures/queen_dark_classic_v1.png";
+import bK from "../../assets/figures/king_dark_classic_v1.png";
 
 export const chessSet1 = {
   wP,
