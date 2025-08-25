cd3ae0eb7ec3996810e4aac16e9c02c3790b3cc8
Konstantin Beregovoy <beregovyyk@gmail.com>
2025-08-26 01:23:36 +0300

    update avatars

--- src/components/AccountButton/AccountButton.tsx
+++ src/components/AccountButton/AccountButton.tsx
@@ -1,12 +1,17 @@
-import React from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { StyledAccountButton } from "./styles";
 import { RootState } from "src/store";
 
 // Import avatar images
-import userBoy from "src/assets/avatars/user_boy.png";
-import userGirl from "src/assets/avatars/user_girl.png";
+import boyAvatar1 from "src/assets/avatars/boy_avatar_1.png";
+import boyAvatar2 from "src/assets/avatars/boy_avatar_2.png";
+import boyAvatar3 from "src/assets/avatars/boy_avatar_3.png";
+import boyAvatar4 from "src/assets/avatars/boy_avatar_4.png";
+import girlAvatar1 from "src/assets/avatars/girl_avatar_1.png";
+import girlAvatar2 from "src/assets/avatars/girl_avatar_2.png";
+import girlAvatar3 from "src/assets/avatars/girl_avatar_3.png";
+import girlAvatar4 from "src/assets/avatars/girl_avatar_4.png";
 import userX from "src/assets/avatars/user_x.png";
 
 interface AccountButtonProps {
@@ -16,21 +21,35 @@ interface AccountButtonProps {
 function AccountButton({ className }: AccountButtonProps) {
   const { t, i18n } = useTranslation();
   const user = useSelector((state: RootState) => state.settings.user);
-
   // Determine which avatar to show based on gender
   const getAvatarImage = (): string => {
     if (!user?.profile?.gender) {
       return userX; // Default for no gender set
     }
 
-    switch (user.profile.gender) {
-      case "male":
-        return userBoy;
-      case "female":
-        return userGirl;
-      default:
-        return userX; // For "prefer_not_to_say" and any other cases
+    if (user.profile.gender === "male") {
+      if (user.profile.avatar === "avatar1") {
+        return boyAvatar1;
+      } else if (user.profile.avatar === "avatar2") {
+        return boyAvatar2;
+      } else if (user.profile.avatar === "avatar3") {
+        return boyAvatar3;
+      } else if (user.profile.avatar === "avatar4") {
+        return boyAvatar4;
+      }
+    } else if (user.profile.gender === "female") {
+      if (user.profile.avatar === "avatar1") {
+        return girlAvatar1;
+      } else if (user.profile.avatar === "avatar2") {
+        return girlAvatar2;
+      } else if (user.profile.avatar === "avatar3") {
+        return girlAvatar3;
+      } else if (user.profile.avatar === "avatar4") {
+        return girlAvatar4;
+      }
     }
+
+    return userX; // Default fallback
   };
 
   return (
